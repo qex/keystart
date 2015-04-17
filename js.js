78ed34c78c038f2ca1fccc6bb028752e7f40e52a
Talent Mux <tmux@qq.com>
2015-04-17 19:19:06 +0800

    source code and license

--- js.js
+++ js.js
@@ -1,1 +1,119 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7(\'V\'!=I("W")){8("1x","h://m.1y.n");8("1z","h://m.1A.n");8("1B","h://m.1C.n");8("1D","h://m.1E.n");8("1F","h://m.1G.n");8("1H","h://m.1I.n");8("1J","h://m.1K.n");8("W","V")};5 j={};1L(5 i=1M;i<=1N;i++){5 q=X.Y(i);5 v=I("J"+q);7(v!=1O&&v!=\'\'&&K(v)!=\'L\'){j[q]=v;$("#k"+q).Z(\'<t w="\'+q+\'" r="10" x="\'+M(v)+\'" 11="12">\')}};$(N).1P(g(a){7(a.1Q){l O}5 b=X.Y(a.1R);$("#k"+b).y(\'A\',\'#13\');14(\'$("#k\'+b+\'").y("A","#15");\',1S);7(j[b]==\'\'||K(j[b])==\'L\'){$("#16").17(\'找不到这个按键的配置,注意切换您的输入法哦~~~\');14(\'$("#16").17("");\',1T)}18{P.19.1a=j[b]}});$("#1b 1c").1U(g(){$("#B").C($(Q).1d(\'w\').1e("k",""));$(Q).1V(\'<1f r="1g"><D><a 1h="R()" r="R" 1i="删除"><t x="1j/1W-1k.1l"></a></D><D><a 1h="1m()" r="1n" 1i="编辑"><t x="1j/1n-1k.1l"></a></D></1f>\')}).1X(g(){$("#B").C(\'\');$(".1g").S()});$("#1b 1c").1Y(g(){5 a=$(Q).1d(\'w\').1e(\'k\',\'\');7(j[a]==\'\'||K(j[a])==\'L\'){}18{P.19.1a=j[a]}});g R(){5 a=$("#B").C();j[a]=\'\';$("#"+a).S();1o("J"+a);l O};g 1m(){5 a=$("#B").C();$("#k"+a).y(\'A\',\'#13\');5 u=P.1Z("请输入键位["+a+"]对应的网站地址","");$("#k"+a).y(\'A\',\'#15\');7(u.p(\'h://\')==-1&&u.p(\'1p://\')==-1){u=\'h://\'+u};7(!1q(u)){20(\'网站地址输入错误!请核对\');l};j[a]=u;$("#"+a).S();$("#k"+a).Z(\'<t w="\'+a+\'" r="10" x="\'+M(u)+\'" 11="12">\');8("J"+a,u);l O};g M(a){5 s=a.p("//");T=a.E(s+2);5 b=T.p("/");7(b==-1){b=T.F};l a.E(0,b+s+2)+\'/21.22\'};g 8(a,b,c,d,e){5 f=U 1r();f.1s(f.1t()+(23*25*24*1u*1u*1v));N.1w=a+"="+26(b)+((f)?"; 27="+f.28():"")+"; 29=/"};g 1o(a){G=U 1r();G.1s(G.1t()-(2a*1v));8(a,"",G)};g I(a){5 b=N.1w;a+="=";5 i=0;2b(i<b.F){5 c=i+a.F;7(b.E(i,c)==a){5 d=b.p(";",c);7(d==-1)d=b.F;l 2c(b.E(c,d))};i=b.p(" ",i)+1;7(i==0)2d};l};g 1q(a){5 b="^((1p|h|2e|2f|2g)?://)"+"?(([0-o-H!~*\'().&=+$%-]+: )?[0-o-H!~*\'().&=+$%-]+@)?"+"(([0-9]{1,3}.){3}[0-9]{1,3}"+"|"+"([0-o-H!~*\'()-]+.)*"+"([0-o-z][0-o-z-]{0,2h})?[0-o-z]."+"[a-z]{2,6})"+"(:[0-9]{1,4})?"+"((/?)|"+"(/[0-o-H!~*\'().;?:@&=+$,%#-]+)+/?)$";5 c=U 2i(b);l!!c.2j(a)};',62,144,'|||||var||if|setCookie||||||||function|http||urlcache|LI_|return|www|com|9a|indexOf|code|class||img|||id|src|css||background|tempdate|val|span|substring|length|expdate|z_|getCookie|_|typeof|undefined|getico|document|false|window|this|del|remove|temp|new|OVER|FIRSTTIME|String|fromCharCode|prepend|fav|align|center|ccf|setTimeout|fff|message|html|else|location|href|main|li|attr|replace|div|oper|onclick|title|images|icon|png|update|edit|deleteCookie|https|IsURL|Date|setTime|getTime|60|1000|cookie|_B|baidu|_G|google|_Q|qq|_T|taobao|_V|v2ex|_W|weibo|_Y|youku|for|48|90|null|keyup|ctrlKey|keyCode|300|2000|mouseenter|append|delete|mouseleave|click|prompt|alert|favicon|ico|100||365|escape|expires|toGMTString|path|86400|while|decodeURIComponent|break|ftp|rtsp|mms|61|RegExp|test'.split('|'),0,{}))
+if ( 'OVER' != getCookie("FIRSTTIME") ) {
+    setCookie("_B", "http://www.baidu.com");
+    setCookie("_G", "http://www.google.com");
+    setCookie("_Q", "http://www.qq.com");
+    setCookie("_T", "http://www.taobao.com");
+    setCookie("_V", "http://www.v2ex.com");    
+    setCookie("_W", "http://www.weibo.com");
+    setCookie("_Y", "http://www.youku.com");
+    setCookie("FIRSTTIME", "OVER");
+};
+
+var urlcache = {};
+
+for (var i = 48; i <= 90; i++) {
+    var code = String.fromCharCode(i);
+    var v = getCookie("_" + code);
+    if (v != null && v != '' && typeof(v) != 'undefined') {
+        urlcache[code] = v;
+        $("#LI_" + code).prepend('<img id="' + code + '" class="fav" src="' + getico(v) + '" align="center">')
+    }
+};
+
+$(document).keyup(function(ev) {
+    if (ev.ctrlKey) return false;
+    var code = String.fromCharCode(ev.keyCode);
+    $("#LI_" + code).addClass("active");
+    setTimeout('$("#LI_' + code + '").removeClass("active");', 300);
+    if (urlcache[code] == '' || typeof(urlcache[code]) == 'undefined') {
+        $("#message").html('找不到这个按键的配置,注意切换您的输入法哦~~~');
+        setTimeout('$("#message").html("");', 2000)
+    } else window.location.href = urlcache[code];
+});
+
+$("#main li").mouseenter(function() {
+    $("#tempdate").val($(this).attr('id').replace("LI_", ""));
+    $(this).append('<div class="oper"><span><a onclick="return del()" class="del" title="删除"><img src="images/delete-icon.png"></a></span><span><a onclick="return update()" class="edit" title="编辑"><img src="images/edit-icon.png"></a></span></div>')
+}).mouseleave(function() {
+    $("#tempdate").val('');
+    $(".oper").remove()
+});
+
+$("#main li").click(function() {
+    var code = $(this).attr('id').replace('LI_', '');
+    if (urlcache[code] != '' && typeof(urlcache[code]) != 'undefined') {
+        window.location.href = urlcache[code];
+    }
+});
+
+function del() {
+    var code = $("#tempdate").val();
+    urlcache[code] = '';
+    $("#" + code).remove();
+    deleteCookie("_" + code);
+    return false;
+};
+
+function update() {
+    var code = $("#tempdate").val();
+    $("#LI_" + code).css('background', '#ccf');
+    var u = window.prompt("请输入键位 [" + code + "] 对应的网站地址", "");
+    $("#LI_" + code).css('background', '#fff');
+    if (u.indexOf('http://') == -1 && u.indexOf('https://') == -1) {
+        u = 'http://' + u
+    };
+    if (!IsURL(u)) {
+        alert('网站地址输入错误!请核对');
+        return false;
+    };
+    urlcache[code] = u;
+    $("#" + code).remove();
+    $("#LI_" + code).prepend('<img id="' + code + '" class="fav" src="' + getico(u) + '" align="center">');
+    setCookie("_" + code, u);
+    return true;
+};
+
+function getico(url) {
+    var s = url.indexOf("//");
+    temp = url.substring(s + 2);
+    var s1 = temp.indexOf("/");
+    if (s1 == -1) {
+        s1 = temp.length
+    };
+    return url.substring(0, s1 + s + 2) + '/favicon.ico'
+};
+
+function setCookie(name, value, path, domain, secure) {
+    var expdate = new Date();
+    expdate.setTime(expdate.getTime() + (100 * 365 * 24 * 60 * 60 * 1000));
+    document.cookie = name + "=" + escape(value) + ((expdate) ? "; expires=" + expdate.toGMTString() : "") + "; path=/";
+};
+
+function deleteCookie(name) {
+    expdate = new Date();
+    expdate.setTime(expdate.getTime() - (86400 * 1000));
+    setCookie(name, "", expdate)
+};
+
+function getCookie(name) {
+    var bikky = document.cookie;
+    name += "=";
+    var i = 0;
+    while (i < bikky.length) {
+        var offset = i + name.length;
+        if (bikky.substring(i, offset) == name) {
+            var endstr = bikky.indexOf(";", offset);
+            if (endstr == -1) endstr = bikky.length;
+            return decodeURIComponent(bikky.substring(offset, endstr))
+        };
+        i = bikky.indexOf(" ", i) + 1;
+        if (i == 0) break
+    };
+    return
+};
+
+function IsURL(str_url) {
+    var strRegex = "^((https|http|ftp|rtsp|mms)?://)" + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" + "(([0-9]{1,3}.){3}[0-9]{1,3}" + "|" + "([0-9a-z_!~*'()-]+.)*" + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]." + "[a-z]{2,6})" + "(:[0-9]{1,4})?" + "((/?)|" + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+    var re = new RegExp(strRegex);
+    return !!re.test(str_url);
+};
